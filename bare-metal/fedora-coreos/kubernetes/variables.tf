a4490bbeb9e219c616c384e6ad541093cd7ea73a
guislander <lynn.d.calvo@gmail.com>
2021-05-09 12:20:44 -0500

    updated node map descriptions

--- bare-metal/fedora-coreos/kubernetes/variables.tf
+++ bare-metal/fedora-coreos/kubernetes/variables.tf
@@ -37,7 +37,7 @@ variable "controllers" {
   }))
   description = <<EOD
 List of controller machine details (unique name, identifying MAC address, FQDN, install device)
-[{ name = "node1", mac = "52:54:00:a1:9c:ae", domain = "node1.example.com", "nvme0n1"}]
+[{ name = "node1", mac = "52:54:00:a1:9c:ae", domain = "node1.example.com", install_dev = "nvme0n1"}]
 EOD
 }
 
@@ -51,8 +51,8 @@ variable "workers" {
   description = <<EOD
 List of worker machine details (unique name, identifying MAC address, FQDN, install device)
 [
-  { name = "node2", mac = "52:54:00:b2:2f:86", domain = "node2.example.com", "nvme0n1"},
-  { name = "node3", mac = "52:54:00:c3:61:77", domain = "node3.example.com", "nvme4n1"}
+  { name = "node2", mac = "52:54:00:b2:2f:86", domain = "node2.example.com", install_dev = "nvme0n1"},
+  { name = "node3", mac = "52:54:00:c3:61:77", domain = "node3.example.com", install_dev = "nvme4n1"}
 ]
 EOD
 }
